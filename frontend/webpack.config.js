72ff805c0480fa90b6d4f3ffc11eaa8775cb3289
Magma <raymondamoateng@gmail.com>
2022-07-19 13:54:16 +0100

    Debugging

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   target: 'web',
   resolve: {
     extensions: ['.js', '.ts', '.tsx'],
-    // Fix webpack's default behavior to not load packages with jsnext:main module
+    // Fix webpack's default behavior to not load packagess with jsnext:main module
     // (jsnext:main directs not usually distributable es6 format, but es6 sources)
     mainFields: ['module', 'browser', 'main'],
     alias: {
